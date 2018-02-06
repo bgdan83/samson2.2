997241d54b8a7c057835e5c97275cb7fe48975e2
Андрей <@192.168.56.1>
2018-02-06 13:46:33 +0300

    task4.2

---
+++ task4.2/partners/.left.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Условия сотрудничества", 
+		"/partners/index.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Расписание мероприятий", 
+		"/partners/schedule-of-events/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Как стать партнером", 
+		"/partners/how-to-become-a-partner.php", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
