8c3d3192d8242358e4206447adf71d885fba1ed1
bgdan83 <maklev@mail.ru>
2018-06-02 16:45:12 +0400

    fixed

--- task5/urlrewrite.php
+++ task5/urlrewrite.php
@@ -3,7 +3,6 @@ $arUrlRewrite = array(
 	array(
 		"CONDITION" => "#^/admin/user_new/([a-zA-Z0-9\\.\\-_]+)/?.*#",
 		"RULE" => "LOGIN=\$1",
-		"RULE" => "LOGIN=\$1",
 		"PATH" => "/admin/users/detail.php",
 	),
 	array(
